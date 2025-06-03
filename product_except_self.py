58af8cccc236c764bcc3d3d4b2277b39862ade5c
BTR <thilakraj1301@gmail.com>
2025-06-02 18:36:14 -0700

    Create product_except_self.py

---
+++ product_except_self.py
@@ -0,0 +1,22 @@
+# Time Complexity
+# The time complexity is O(n) because we make two passes over the input array.
+# Space Complexity
+# The space complexity is O(1) because we only use a constant amount of space (not counting the output array).
+
+def productExceptSelf(nums):
+    n = len(nums)
+    output = [1] * n
+
+    # Calculate product of all numbers to the left
+    left_product = 1
+    for i in range(n):
+        output[i] *= left_product
+        left_product *= nums[i]
+
+    # Calculate product of all numbers to the right
+    right_product = 1
+    for i in range(n-1, -1, -1):
+        output[i] *= right_product
+        right_product *= nums[i]
+
+    return output
